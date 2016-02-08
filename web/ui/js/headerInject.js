61de1fe95c665e5cfb3c8541af3f17d89e5dc75b
brian-rose-sp <brian.rose@sailpoint.com>
2016-02-08 14:17:10 -0600

    Updated to show example of dynamic url in the JS

--- web/ui/js/headerInject.js
+++ web/ui/js/headerInject.js
@@ -1,8 +1,23 @@
+
+/*
+
+Other available values to build paths, etc. that are obtained from the server and set in the PluginFramework JS object
+
+PluginFramework.PluginBaseEndpointName = '#{pluginFramework.basePluginEndpointName}';
+PluginFramework.PluginEndpointRoot = '#{pluginFramework.basePluginEndpointName}/#{pluginFramework.basePluginEndpointName}';
+PluginFramework.PluginFolderName = '#{pluginFramework.pluginFolderName}';
+PluginFramework.CurrentPluginUniqueName = '#{pluginFramework.uniqueName}';
+PluginFramework.CsrfToken = Ext.util.Cookies.get('CSRF-TOKEN');
+
+ */
+
+
+var helloWorldUrl = SailPoint.CONTEXT_PATH + '/pluginPage.jsf?pn=HelloWorld';
 jQuery(document).ready(function(){
 	jQuery("ul.navbar-right li:first")
 		.before(
 				'<li>' +
-				'		<a href="/identityiq/pluginPage.jsf?pn=HelloWorld" tabindex="0" role="menuitem">' +
+				'		<a href="' + helloWorldUrl + '" tabindex="0" role="menuitem">' +
 				'			<i role="presenation" class="fa fa-exclamation fa-lg"></i>' +
 				'		</a>' +
 				'</li>'
