74a34d5fa1d9370cf31c2843d877abc67361210f
Maximilian Roquemore <maximilian.roquemore@mroque-mbp13.sailpoint.com>
2016-06-22 14:12:46 -0500

    work

--- web/ui/js/ipmapper.js
+++ web/ui/js/ipmapper.js
@@ -14,7 +14,7 @@ var IPMapper = {
 		var latlng = new google.maps.LatLng(0, 0);
 		//set Map options
 		var mapOptions = {
-			zoom: 1,
+			zoom: 11,
 			center: latlng,
 			styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}],
 			mapTypeId: IPMapper.mapTypeId
